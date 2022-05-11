d7c8d0e59ee6617ec2c58f5e3943da9d23f6a9c8
김이영 <65011438+kimleeyoung@users.noreply.github.com>
2022-05-11 16:22:10 +0900

    Add files via upload

---
+++ PublicCertificate/PublicCertificate/BringCert.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace PublicCertificate
+{
+    partial class BringCert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.UltraWinGrid.UltraGridBand ultraGridBand1 = new Infragistics.Win.UltraWinGrid.UltraGridBand("", -1);
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance5 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance6 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance7 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance8 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance9 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance10 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance11 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance12 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance13 = new Infragistics.Win.Appearance();
+            this.mccSearchTextBox1 = new Mcc.Series.Controls.MccSearchTextBox();
+            this.grdSavedCert = new Mcc.Series.Controls.MccGrid();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlbasetop.SuspendLayout();
+            this.pnlbasemain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxbasetitle)).BeginInit();
+            this.pnalbasetopbtn.SuspendLayout();
+            this.pnlbasebutton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdSavedCert)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlbasemain
+            // 
+            this.pnlbasemain.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // btnbaseF8
+            // 
+            this.btnbaseF8.FlatAppearance.BorderSize = 0;
+            this.btnbaseF8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF9
+            // 
+            this.btnbaseF9.FlatAppearance.BorderSize = 0;
+            this.btnbaseF9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF10
+            // 
+            this.btnbaseF10.FlatAppearance.BorderSize = 0;
+            this.btnbaseF10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF7
+            // 
+            this.btnbaseF7.FlatAppearance.BorderSize = 0;
+            this.btnbaseF7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF5
+            // 
+            this.btnbaseF5.FlatAppearance.BorderSize = 0;
+            this.btnbaseF5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF6
+            // 
+            this.btnbaseF6.FlatAppearance.BorderSize = 0;
+            this.btnbaseF6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // mccSearchTextBox1
+            // 
+            this.mccSearchTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mccSearchTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.mccSearchTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.mccSearchTextBox1.Name = "mccSearchTextBox1";
+            this.mccSearchTextBox1.Size = new System.Drawing.Size(996, 24);
+            this.mccSearchTextBox1.TabIndex = 0;
+            // 
+            // grdSavedCert
+            // 
+            appearance1.BackColor = System.Drawing.SystemColors.Window;
+            appearance1.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.grdSavedCert.DisplayLayout.Appearance = appearance1;
+            ultraGridBand1.Override.AllowRowFiltering = Infragistics.Win.DefaultableBoolean.False;
+            ultraGridBand1.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            ultraGridBand1.Override.RowSelectorHeaderStyle = Infragistics.Win.UltraWinGrid.RowSelectorHeaderStyle.SeparateElement;
+            this.grdSavedCert.DisplayLayout.BandsSerializer.Add(ultraGridBand1);
+            this.grdSavedCert.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.grdSavedCert.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.False;
+            appearance2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            appearance2.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance2.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance2.BorderColor = System.Drawing.SystemColors.Window;
+            this.grdSavedCert.DisplayLayout.GroupByBox.Appearance = appearance2;
+            appearance3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.grdSavedCert.DisplayLayout.GroupByBox.BandLabelAppearance = appearance3;
+            this.grdSavedCert.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.grdSavedCert.DisplayLayout.GroupByBox.Hidden = true;
+            appearance5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            appearance5.BackColor2 = System.Drawing.SystemColors.Control;
+            appearance5.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance5.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.grdSavedCert.DisplayLayout.GroupByBox.PromptAppearance = appearance5;
+            this.grdSavedCert.DisplayLayout.MaxColScrollRegions = 1;
+            this.grdSavedCert.DisplayLayout.MaxRowScrollRegions = 1;
+            appearance6.BackColor = System.Drawing.SystemColors.Window;
+            appearance6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grdSavedCert.DisplayLayout.Override.ActiveCellAppearance = appearance6;
+            appearance7.BackColor = System.Drawing.SystemColors.Highlight;
+            appearance7.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.grdSavedCert.DisplayLayout.Override.ActiveRowAppearance = appearance7;
+            this.grdSavedCert.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.Dotted;
+            this.grdSavedCert.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dotted;
+            appearance8.BackColor = System.Drawing.SystemColors.Window;
+            this.grdSavedCert.DisplayLayout.Override.CardAreaAppearance = appearance8;
+            appearance9.BorderColor = System.Drawing.Color.Silver;
+            appearance9.TextTrimming = Infragistics.Win.TextTrimming.EllipsisCharacter;
+            this.grdSavedCert.DisplayLayout.Override.CellAppearance = appearance9;
+            this.grdSavedCert.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            this.grdSavedCert.DisplayLayout.Override.CellPadding = 0;
+            appearance10.BackColor = System.Drawing.SystemColors.Control;
+            appearance10.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance10.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance10.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance10.BorderColor = System.Drawing.SystemColors.Window;
+            this.grdSavedCert.DisplayLayout.Override.GroupByRowAppearance = appearance10;
+            appearance11.TextHAlignAsString = "Left";
+            this.grdSavedCert.DisplayLayout.Override.HeaderAppearance = appearance11;
+            this.grdSavedCert.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti;
+            this.grdSavedCert.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand;
+            appearance12.BackColor = System.Drawing.SystemColors.Window;
+            appearance12.BorderColor = System.Drawing.Color.Silver;
+            this.grdSavedCert.DisplayLayout.Override.RowAppearance = appearance12;
+            this.grdSavedCert.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.False;
+            appearance13.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grdSavedCert.DisplayLayout.Override.TemplateAddRowAppearance = appearance13;
+            this.grdSavedCert.DisplayLayout.Override.TipStyleHeader = Infragistics.Win.UltraWinGrid.TipStyle.Hide;
+            this.grdSavedCert.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill;
+            this.grdSavedCert.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate;
+            this.grdSavedCert.DisplayLayout.ViewStyleBand = Infragistics.Win.UltraWinGrid.ViewStyleBand.OutlookGroupBy;
+            this.grdSavedCert.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdSavedCert.DoNotAutomaticallyUpdate = false;
+            this.grdSavedCert.ExceptCellChangeEventColumns = new string[0];
+            this.grdSavedCert.Location = new System.Drawing.Point(3, 33);
+            this.grdSavedCert.MccLastLayoutMemory = true;
+            this.grdSavedCert.MccNonHeaderLineColor = false;
+            this.grdSavedCert.Name = "grdSavedCert";
+            this.grdSavedCert.Size = new System.Drawing.Size(996, 532);
+            this.grdSavedCert.TabIndex = 1;
+            this.grdSavedCert.Text = "mccGrid1";
+            this.grdSavedCert.UpdateMode = Infragistics.Win.UltraWinGrid.UpdateMode.OnCellChange;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.mccSearchTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grdSavedCert, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1002, 568);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // BringCert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 613);
+            this.Name = "BringCert";
+            this.Text = this.Name;
+            this.pnlbasetop.ResumeLayout(false);
+            this.pnlbasetop.PerformLayout();
+            this.pnlbasemain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxbasetitle)).EndInit();
+            this.pnalbasetopbtn.ResumeLayout(false);
+            this.pnlbasebutton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdSavedCert)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Mcc.Series.Controls.MccSearchTextBox mccSearchTextBox1;
+        public Mcc.Series.Controls.MccGrid grdSavedCert;
+    }
+}
---
+++ PublicCertificate/PublicCertificate/BringCert.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Mcc.Series.Ui.Type;
+using Mcc.Series.Controls.Enum;
+using Mcc.Series.Common.Enum;
+using Infragistics.Win.UltraWinGrid;
+using static Mcc.Series.Ui.PopUp.CodeHelp;
+
+namespace PublicCertificate
+{
+    public partial class BringCert : Mcc.Series.Ui.FormBase
+    {
+        #region - member variable -
+        private DataTable _dt;
+        private int _intColCount = 2;
+        private string[] _colNames;
+        private int[] _colSizes;
+        private int _searchType = 0;
+        private string[] _usecolumns;
+        private string _sSearchTxt = string.Empty;
+        private string _strName = string.Empty;
+        private bool _multiselect = false;
+        private SearchMethod _searchmethod = SearchMethod.KeyPress;
+
+
+        public DataTable SetDataTable
+        {
+            get
+            {
+                return _dt;
+            }
+            set
+            {
+                _dt = value;
+
+            }
+        }
+        public string ColNames
+        {
+            set
+            {
+                char[] del = { ',' };
+                _colNames = value.Split(del);
+            }
+        }
+        public string ColSizes
+        {
+            set
+            {
+                char[] del = { ',' };
+                string[] sTmp = value.Split(del);
+                _colSizes = new int[sTmp.Length];
+                for (int ii = 0; ii < sTmp.Length; ii++)
+                {
+                    _colSizes[ii] = Convert.ToInt32(sTmp[ii]);
+                }
+            }
+        }
+        public int ColCount
+        {
+            get
+            {
+                return _intColCount;
+            }
+            set
+            {
+                _intColCount = value;
+            }
+        }
+        public string UseColumns
+        {
+            set
+            {
+                char[] del = { ',' };
+                string[] sTmp = value.Split(del);
+                _usecolumns = new string[sTmp.Length];
+                for (int ii = 0; ii < sTmp.Length; ii++)
+                {
+                    _usecolumns[ii] = sTmp[ii].Trim();
+                }
+            }
+        }
+        public int searchType
+        {
+            get
+            {
+                return _searchType;
+            }
+            set
+            {
+                _searchType = value;
+            }
+        }
+        public string ReturnName
+        {
+            get
+            {
+                return _strName;
+            }
+            set
+            {
+                _strName = value;
+            }
+        }
+        #endregion
+
+        #region - constructor -
+        public BringCert()
+        {
+            InitializeComponent();
+
+            this.SetBaseButtonHide(new EnumBaseButtonChoose[] { EnumBaseButtonChoose.btnbaseF9
+                                                              , EnumBaseButtonChoose.btnbaseF7
+                                                              , EnumBaseButtonChoose.btnbaseF8
+                                                              , EnumBaseButtonChoose.btnbaseF9
+                                                              , EnumBaseButtonChoose.btnbaseF10 });
+
+            this.Load += new EventHandler(BringCert_Load);
+            this.FormClosing += new FormClosingEventHandler(BringCert_FormClosing);
+
+            grdSavedCert.DoubleClick += GrdSavedCert_DoubleClick;
+            mccSearchTextBox1.KeyPress += MccSearchTextBox1_KeyPress;
+            mccSearchTextBox1.KeyDown += MccSearchTextBox1_KeyDown;
+        }
+
+        private void MccSearchTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyData.Equals(Keys.Up))
+                {
+                    grdSavedCert.Focus();
+                }
+                else if (e.KeyData.Equals(Keys.Down))
+                {
+                    if (grdSavedCert.Rows.Count > 0)
+                        grdSavedCert.Rows[0].Activate();
+                    grdSavedCert.Focus();
+                }
+                else if (e.KeyData.Equals(Keys.Enter))
+                {
+                    DataTable dt = this.SetDataTable;
+
+                    if (!string.IsNullOrEmpty(mccSearchTextBox1.Text))
+                    {
+                        if (mccSearchTextBox1.Text != _sSearchTxt)
+                        {
+                            _sSearchTxt = mccSearchTextBox1.Text;
+
+                            if (dt.Select(" " + dt.Columns[0].ColumnName.ToString() + " like '%" + mccSearchTextBox1.Text + "%' or " + dt.Columns[1].ColumnName.ToString() + " like'%" + mccSearchTextBox1.Text + "%' ").Count() > 0)
+                            {
+                                DataTable dt1 = dt.Select(" " + dt.Columns[0].ColumnName.ToString() + " like '%" + mccSearchTextBox1.Text + "%' or " + dt.Columns[1].ColumnName.ToString() + " like'%" + mccSearchTextBox1.Text + "%' ").CopyToDataTable();
+                                grdSavedCert.FillData(dt1);
+                            }
+                            else
+                                grdSavedCert.RowsClearAll();
+                        }
+                        else
+                        {
+                            if (grdSavedCert.Rows.Count > 0)
+                                grdSavedCert.Rows[0].Activate();
+                            grdSavedCert.Focus();
+                        }
+                    }
+                    else
+                    {
+                        grdSavedCert.FillData(dt.Copy());
+                        if (grdSavedCert.Rows.Count > 0)
+                            grdSavedCert.Rows[0].Activate();
+                        grdSavedCert.Focus();
+                    }
+
+                }
+            }
+            catch// (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void MccSearchTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (_searchmethod == SearchMethod.EnterKey && e.KeyChar == 13)
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                //MessageBox.Show(txtSearch.Text);
+                foreach (UltraGridRow row in grdSavedCert.Rows)
+                {
+                    if (mccSearchTextBox1.Text.Equals(string.Empty))
+                    {
+                        break;
+                    }
+                    else if (row.Cells[_searchType].Text.StartsWith(mccSearchTextBox1.Text))
+                    {
+                        grdSavedCert.ActiveRow = row;
+                        break;
+                    }
+                }
+
+                this.Cursor = Cursors.Default;
+            }
+        }
+        #endregion
+
+        #region - form events / SetInitialize -
+        private void BringCert_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // To Do..
+                this.SetInitialize();
+                this.SetGridHeader();
+
+                if (_dt.Rows.Count > 0)
+                {
+                    this.Show();
+                    //this.SetGridHeader();
+
+                    grdSavedCert.FillData(_dt.Copy());
+
+                    mccSearchTextBox1.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BringCert_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+        #region - SetInitialize / SetGridHeader -
+        private void SetInitialize()
+        {
+            // To Do..
+            lblbasetitle.Text = "등록된 인증서";
+        }
+
+        private void SetGridHeader()
+        {
+
+            for (int i = 0; i < ((_intColCount <= _dt.Columns.Count) ? _intColCount : _dt.Columns.Count); i++)
+            {
+                if (_colSizes[i].Equals(0))
+                    grdSavedCert.AddColumn(_dt.Columns[i].ToString(), _colNames[i], 50, GridColumnStyle.Default, HiddenType.True, ReadOnlyType.True, GridMaskStyle.None);
+                else
+                    grdSavedCert.AddColumn(_dt.Columns[i].ToString(), _colNames[i], ((_colSizes[i] < 10) ? 50 : _colSizes[i]), GridColumnStyle.Default, HiddenType.False, ReadOnlyType.True, GridMaskStyle.None);
+
+            }
+
+            grdSavedCert.SetGridHeader();
+
+
+            if (!_multiselect) grdSavedCert.SetReadOnlyGridViewModeStyle(false, FilterUIType.Default, false);
+            grdSavedCert.DisplayLayout.AutoFitStyle = Infragistics.Win.UltraWinGrid.AutoFitStyle.ExtendLastColumn;
+        }
+        #endregion
+
+        private void GrdSavedCert_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                this.SelectItemApply();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void SelectItemApply()
+        {
+            if ((grdSavedCert.Rows.Count < 1) || (grdSavedCert.ActiveRow == null)) return;
+
+            string a = grdSavedCert.ActiveRow.Cells[3].Text.Trim();
+            string b = grdSavedCert.ActiveRow.Cells[2].Text.Trim();
+            this.ReturnName = grdSavedCert.ActiveRow.Cells[1].Text.Trim();
+
+            if (a == "만료")
+            {
+                MessageBox.Show("만료된 인증서 입니다. 인증서를 다시 선택해주세요.", "만료된 인증서", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (MessageBox.Show(" 만료일자: " + b + "\r" + "인증서를 선택하겠습니까?", "인증서 선택", MessageBoxButtons.YesNo) == DialogResult.No)
+                    return;
+                else
+                {
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+        }
+
+
+
+        #region - top button events -
+        protected override void btnbaseF5_Click()
+        {
+            base.btnbaseF5_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF6_Click()
+        {
+            base.btnbaseF6_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF7_Click()
+        {
+            base.btnbaseF7_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF8_Click()
+        {
+            base.btnbaseF8_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF9_Click()
+        {
+            base.btnbaseF9_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF10_Click()
+        {
+            base.btnbaseF10_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+    }
+}
