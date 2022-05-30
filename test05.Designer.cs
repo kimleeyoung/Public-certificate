d769fe073cfa0cfc8a50a89b4b4880b8d879c2f1
김이영 <65011438+kimleeyoung@users.noreply.github.com>
2022-05-30 17:58:41 +0900

    Add files via upload

---
+++ test05.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Mcc.Clinic.Common.TEST
+{
+    partial class test05
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance13 = new Infragistics.Win.Appearance();
+            Infragistics.Win.UltraWinGrid.UltraGridBand ultraGridBand1 = new Infragistics.Win.UltraWinGrid.UltraGridBand("", -1);
+            Infragistics.Win.Appearance appearance14 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance16 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance15 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance19 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance22 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance24 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance20 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance18 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance17 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance23 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance21 = new Infragistics.Win.Appearance();
+            this.txtPtntNo = new System.Windows.Forms.TextBox();
+            this.txtPtntNm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPtntAddr = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cbxPtntDept = new Mcc.Series.Controls.MccComboBox();
+            this.grdPtnt = new Mcc.Series.Controls.MccGrid();
+            this.pnlbasetop.SuspendLayout();
+            this.pnlbasemain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxbasetitle)).BeginInit();
+            this.pnalbasetopbtn.SuspendLayout();
+            this.pnlbasebutton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPtnt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlbasetop
+            // 
+            this.pnlbasetop.Size = new System.Drawing.Size(909, 32);
+            // 
+            // pnlbasemain
+            // 
+            this.pnlbasemain.Controls.Add(this.grdPtnt);
+            this.pnlbasemain.Controls.Add(this.cbxPtntDept);
+            this.pnlbasemain.Controls.Add(this.btnDelete);
+            this.pnlbasemain.Controls.Add(this.btnSave);
+            this.pnlbasemain.Controls.Add(this.label4);
+            this.pnlbasemain.Controls.Add(this.label3);
+            this.pnlbasemain.Controls.Add(this.label2);
+            this.pnlbasemain.Controls.Add(this.label1);
+            this.pnlbasemain.Controls.Add(this.txtPtntAddr);
+            this.pnlbasemain.Controls.Add(this.txtPtntNm);
+            this.pnlbasemain.Controls.Add(this.txtPtntNo);
+            this.pnlbasemain.Size = new System.Drawing.Size(909, 270);
+            // 
+            // btnbaseF8
+            // 
+            this.btnbaseF8.FlatAppearance.BorderSize = 0;
+            this.btnbaseF8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF9
+            // 
+            this.btnbaseF9.FlatAppearance.BorderSize = 0;
+            this.btnbaseF9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF10
+            // 
+            this.btnbaseF10.FlatAppearance.BorderSize = 0;
+            this.btnbaseF10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF7
+            // 
+            this.btnbaseF7.FlatAppearance.BorderSize = 0;
+            this.btnbaseF7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF5
+            // 
+            this.btnbaseF5.FlatAppearance.BorderSize = 0;
+            this.btnbaseF5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // btnbaseF6
+            // 
+            this.btnbaseF6.FlatAppearance.BorderSize = 0;
+            this.btnbaseF6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            // 
+            // pnlpopup
+            // 
+            this.pnlpopup.Size = new System.Drawing.Size(909, 1);
+            // 
+            // pnalbasetopbtn
+            // 
+            this.pnalbasetopbtn.Location = new System.Drawing.Point(266, 0);
+            // 
+            // txtPtntNo
+            // 
+            this.txtPtntNo.Location = new System.Drawing.Point(121, 42);
+            this.txtPtntNo.Name = "txtPtntNo";
+            this.txtPtntNo.Size = new System.Drawing.Size(174, 21);
+            this.txtPtntNo.TabIndex = 0;
+            // 
+            // txtPtntNm
+            // 
+            this.txtPtntNm.Location = new System.Drawing.Point(121, 79);
+            this.txtPtntNm.Name = "txtPtntNm";
+            this.txtPtntNm.Size = new System.Drawing.Size(174, 21);
+            this.txtPtntNm.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "환자번호";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "환자명";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "주소";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "진료과";
+            // 
+            // txtPtntAddr
+            // 
+            this.txtPtntAddr.Location = new System.Drawing.Point(121, 115);
+            this.txtPtntAddr.Name = "txtPtntAddr";
+            this.txtPtntAddr.Size = new System.Drawing.Size(174, 21);
+            this.txtPtntAddr.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(43, 192);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 43);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(172, 192);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 43);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;            
+            // 
+            // cbxPtntDept
+            // 
+            this.cbxPtntDept.Font = new System.Drawing.Font("굴림체", 9F);
+            this.cbxPtntDept.FormattingEnabled = true;
+            this.cbxPtntDept.Location = new System.Drawing.Point(121, 150);
+            this.cbxPtntDept.Name = "cbxPtntDept";
+            this.cbxPtntDept.Size = new System.Drawing.Size(174, 20);
+            this.cbxPtntDept.TabIndex = 6;
+            // 
+            // grdPtnt
+            // 
+            appearance13.BackColor = System.Drawing.SystemColors.Window;
+            appearance13.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.grdPtnt.DisplayLayout.Appearance = appearance13;
+            ultraGridBand1.Override.AllowRowFiltering = Infragistics.Win.DefaultableBoolean.False;
+            ultraGridBand1.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            ultraGridBand1.Override.RowSelectorHeaderStyle = Infragistics.Win.UltraWinGrid.RowSelectorHeaderStyle.SeparateElement;
+            this.grdPtnt.DisplayLayout.BandsSerializer.Add(ultraGridBand1);
+            this.grdPtnt.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.grdPtnt.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.False;
+            appearance14.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            appearance14.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance14.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance14.BorderColor = System.Drawing.SystemColors.Window;
+            this.grdPtnt.DisplayLayout.GroupByBox.Appearance = appearance14;
+            appearance16.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.grdPtnt.DisplayLayout.GroupByBox.BandLabelAppearance = appearance16;
+            this.grdPtnt.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.grdPtnt.DisplayLayout.GroupByBox.Hidden = true;
+            appearance15.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            appearance15.BackColor2 = System.Drawing.SystemColors.Control;
+            appearance15.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance15.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.grdPtnt.DisplayLayout.GroupByBox.PromptAppearance = appearance15;
+            this.grdPtnt.DisplayLayout.MaxColScrollRegions = 1;
+            this.grdPtnt.DisplayLayout.MaxRowScrollRegions = 1;
+            appearance19.BackColor = System.Drawing.SystemColors.Window;
+            appearance19.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grdPtnt.DisplayLayout.Override.ActiveCellAppearance = appearance19;
+            appearance22.BackColor = System.Drawing.SystemColors.Highlight;
+            appearance22.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.grdPtnt.DisplayLayout.Override.ActiveRowAppearance = appearance22;
+            this.grdPtnt.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.Dotted;
+            this.grdPtnt.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dotted;
+            appearance24.BackColor = System.Drawing.SystemColors.Window;
+            this.grdPtnt.DisplayLayout.Override.CardAreaAppearance = appearance24;
+            appearance20.BorderColor = System.Drawing.Color.Silver;
+            appearance20.TextTrimming = Infragistics.Win.TextTrimming.EllipsisCharacter;
+            this.grdPtnt.DisplayLayout.Override.CellAppearance = appearance20;
+            this.grdPtnt.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            this.grdPtnt.DisplayLayout.Override.CellPadding = 0;
+            appearance18.BackColor = System.Drawing.SystemColors.Control;
+            appearance18.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance18.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance18.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance18.BorderColor = System.Drawing.SystemColors.Window;
+            this.grdPtnt.DisplayLayout.Override.GroupByRowAppearance = appearance18;
+            appearance17.TextHAlignAsString = "Left";
+            this.grdPtnt.DisplayLayout.Override.HeaderAppearance = appearance17;
+            this.grdPtnt.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti;
+            this.grdPtnt.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand;
+            appearance23.BackColor = System.Drawing.SystemColors.Window;
+            appearance23.BorderColor = System.Drawing.Color.Silver;
+            this.grdPtnt.DisplayLayout.Override.RowAppearance = appearance23;
+            this.grdPtnt.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.False;
+            appearance21.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grdPtnt.DisplayLayout.Override.TemplateAddRowAppearance = appearance21;
+            this.grdPtnt.DisplayLayout.Override.TipStyleHeader = Infragistics.Win.UltraWinGrid.TipStyle.Hide;
+            this.grdPtnt.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill;
+            this.grdPtnt.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate;
+            this.grdPtnt.DisplayLayout.ViewStyleBand = Infragistics.Win.UltraWinGrid.ViewStyleBand.OutlookGroupBy;
+            this.grdPtnt.DoNotAutomaticallyUpdate = false;
+            this.grdPtnt.ExceptCellChangeEventColumns = new string[0];
+            this.grdPtnt.Location = new System.Drawing.Point(317, 42);
+            this.grdPtnt.MccLastLayoutMemory = true;
+            this.grdPtnt.MccNonHeaderLineColor = false;
+            this.grdPtnt.Name = "grdPtnt";
+            this.grdPtnt.Size = new System.Drawing.Size(573, 193);
+            this.grdPtnt.TabIndex = 7;
+            this.grdPtnt.Text = "mccGrid1";
+            this.grdPtnt.UpdateMode = Infragistics.Win.UltraWinGrid.UpdateMode.OnCellChange;
+            // 
+            // test05
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 312);
+            this.Name = "test05";
+            this.Text = this.Name;
+            this.pnlbasetop.ResumeLayout(false);
+            this.pnlbasetop.PerformLayout();
+            this.pnlbasemain.ResumeLayout(false);
+            this.pnlbasemain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxbasetitle)).EndInit();
+            this.pnalbasetopbtn.ResumeLayout(false);
+            this.pnlbasebutton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdPtnt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPtntNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPtntNm;
+        private System.Windows.Forms.TextBox txtPtntAddr;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private Series.Controls.MccComboBox cbxPtntDept;
+        private Series.Controls.MccGrid grdPtnt;
+    }
+}
---
+++ test05.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Mcc.Series.Ui.Type;
+using Mcc.Series.Controls.Enum;
+using Mcc.Series.Common.Enum;
+using Mcc.Series.DataBase;
+
+namespace Mcc.Clinic.Common.TEST
+{
+    public partial class test05 : Mcc.Series.Ui.FormBase
+    {
+        #region - member variable -
+
+        #endregion
+
+        #region - constructor -
+        public test05()
+        {
+            InitializeComponent();
+
+            this.SetBaseButtonHide(new EnumBaseButtonChoose[] { EnumBaseButtonChoose.btnbaseF9
+                                                              , EnumBaseButtonChoose.btnbaseF5
+                                                              , EnumBaseButtonChoose.btnbaseF6
+                                                              , EnumBaseButtonChoose.btnbaseF7
+                                                              , EnumBaseButtonChoose.btnbaseF8
+                                                              , EnumBaseButtonChoose.btnbaseF10 });
+
+            this.Load += new EventHandler(test05_Load);
+            this.FormClosing += new FormClosingEventHandler(test05_FormClosing);
+
+            this.cbxPtntDept.SelectedValueChanged += CbxPtntDept_SelectedValueChanged;
+
+        }
+        
+        #endregion
+
+        #region - form events / SetInitialize -
+        private void test05_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // To Do..
+                this.SetInitialize();
+                this.SetGridHeader();
+                setCombo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void test05_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+        #region - SetInitialize / SetGridHeader -
+        private void SetInitialize()
+        {
+            // To Do..
+            lblbasetitle.Text = "환자등록";
+        }
+
+        private void SetGridHeader()
+        {
+            grdPtnt.AddColumn("ptnt_no", "환자번호", 80, GridColumnStyle.Default, HiddenType.False, ReadOnlyType.NoEdit, GridMaskStyle.None);
+            grdPtnt.AddColumn("ptnt_nm", "환자명", 80, GridColumnStyle.Default, HiddenType.False, ReadOnlyType.NoEdit, GridMaskStyle.None);
+            grdPtnt.AddColumn("addr", "주소", 100, GridColumnStyle.Default, HiddenType.False, ReadOnlyType.NoEdit, GridMaskStyle.None);
+            grdPtnt.AddColumn("att_dept", "진료과", 50, GridColumnStyle.Default, HiddenType.False, ReadOnlyType.NoEdit, GridMaskStyle.None);
+            grdPtnt.SetGridHeader();
+            //grdPtnt.DisplayLayout.AutoFitStyle = Infragistics.Win.UltraWinGrid.AutoFitStyle.ExtendLastColumn;
+        }
+        #endregion
+
+        #region - top button events -
+        protected override void btnbaseF5_Click()
+        {
+            base.btnbaseF5_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF6_Click()
+        {
+            base.btnbaseF6_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF7_Click()
+        {
+            base.btnbaseF7_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF8_Click()
+        {
+            base.btnbaseF8_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF9_Click()
+        {
+            base.btnbaseF9_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void btnbaseF10_Click()
+        {
+            base.btnbaseF10_Click();
+
+            try
+            {
+                // To Do..
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+        private void setCombo()
+        {
+            Mcc.Series.DataBase.DBMessage sMsg = new Mcc.Series.DataBase.DBMessage();
+            sMsg.SqlStatement = @"select dept_cd, dept_nm from hz_mst_dept_test;";
+
+            DataTable dt = this.FillDataSet(sMsg).Tables[0];
+
+            if (dt.Rows.Count > 0)                                                    
+            {
+                cbxPtntDept.FillData(dt, "dept_nm", "dept_cd", AddingItemMode.None);                      
+            }
+            else
+            {
+                MessageBox.Show("환자에 대한 정보가 없습니다. 환자정보를 등록해주세요");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            InserData();
+            GetData();
+        }
+
+        private void InserData()
+        {
+            DataTable dt = new DataTable();
+            DBMessage msg = new DBMessage();
+            msg.SqlStatement = @"INSERT INTO public.hz_mst_ptnt_test
+                                 (ptnt_no, ptnt_nm, addr, att_dept)
+                                 VALUES(@ptnt_no, @ptnt_nm, @addr, @att_dept);";
+
+            msg.AddParameter("ptnt_no", txtPtntNo.Text);
+            msg.AddParameter("ptnt_nm", txtPtntNm.Text);
+            msg.AddParameter("addr", txtPtntAddr.Text);
+            msg.AddParameter("att_dept", cbxPtntDept.SelectedValue.ToString());
+
+            this.ExecuteNonQuery(msg);
+            dt = this.FillDataSet(msg).Tables[0];
+
+            if(dt.Rows.Count > 0)
+            {
+                grdPtnt.FillData(dt);
+            }
+        }
+        private void GetData()
+        {
+
+        }
+
+        private void CbxPtntDept_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cbxPtntDept.SelectedValue == null) return;
+
+            if (((Control)sender).Visible)
+            {
+
+            }
+        }
+    }
+}
